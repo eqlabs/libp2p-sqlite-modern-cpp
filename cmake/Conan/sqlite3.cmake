e2bf537c5bceec8980c6ac88385f84b084ba7e60
Dmytro Gordon <g-d@ukr.net>
2022-08-30 12:59:42 +0300

    Fix package discovery

--- cmake/Conan/sqlite3.cmake
+++ cmake/Conan/sqlite3.cmake
@@ -1,6 +1,5 @@
 add_conan_package(sqlite3 3.39.2)
-set(SQLITE_PACKAGE_NAME SQLite3)
-find_package(${SQLITE_PACKAGE_NAME} MODULE REQUIRED)
+find_package(SQLite3 MODULE REQUIRED)
 include_directories(${SQLite3_INCLUDE_DIRS})
 
 set_target_properties(SQLite::SQLite3 PROPERTIES IMPORTED_GLOBAL TRUE)
--- cmake/Hunter/sqlite3.cmake
+++ cmake/Hunter/sqlite3.cmake
@@ -1,3 +1,2 @@
 hunter_add_package(sqlite3)
-set(SQLITE_PACKAGE_NAME sqlite3)
-find_package(${SQLITE_PACKAGE_NAME} CONFIG REQUIRED)
+find_package(sqlite3 CONFIG REQUIRED)

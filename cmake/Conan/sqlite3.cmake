4ac6ca9edcd9a91cef5d6b3cdd4d7c388d0f81c8
Dmytro Gordon <g-d@ukr.net>
2022-08-30 11:59:20 +0300

    Fix SQLite lib name

--- cmake/Conan/sqlite3.cmake
+++ cmake/Conan/sqlite3.cmake
@@ -2,5 +2,4 @@ add_conan_package(sqlite3 3.39.2)
 find_package(SQLite3 MODULE REQUIRED)
 include_directories(${SQLite3_INCLUDE_DIRS})
 
-set_target_properties(SQLite::SQLite3 PROPERTIES IMPORTED_GLOBAL TRUE)
-add_library(sqlite3::sqlite3 ALIAS SQLite::SQLite3)
+set(SQLITE_LIB SQLite::SQLite3)
--- cmake/Hunter/sqlite3.cmake
+++ cmake/Hunter/sqlite3.cmake
@@ -1,2 +1,4 @@
 hunter_add_package(sqlite3)
 find_package(sqlite3 CONFIG REQUIRED)
+
+set(SQLITE_LIB sqlite3::sqlite3)
